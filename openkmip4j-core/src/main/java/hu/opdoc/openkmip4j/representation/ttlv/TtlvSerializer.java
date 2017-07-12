8a61574db59c5aa762aaaf5780b3cd71772959eb
opdoc <orvos.peter76@gmail.com>
2017-07-12 20:29:06 +0200

    I've finished the serialization of primitives.

--- openkmip4j-core/src/main/java/hu/opdoc/openkmip4j/representation/ttlv/TtlvSerializer.java
+++ openkmip4j-core/src/main/java/hu/opdoc/openkmip4j/representation/ttlv/TtlvSerializer.java
@@ -1,21 +1,32 @@
 package hu.opdoc.openkmip4j.representation.ttlv;
 
 import hu.opdoc.openkmip4j.KmipException;
+import hu.opdoc.openkmip4j.KmipRuntimeException;
 import hu.opdoc.openkmip4j.primitives.*;
+import hu.opdoc.openkmip4j.primitives.BigInteger;
 import hu.opdoc.openkmip4j.primitives.Boolean;
 import hu.opdoc.openkmip4j.primitives.Integer;
 import hu.opdoc.openkmip4j.representation.KmipSerializer;
 import hu.opdoc.openkmip4j.representation.KmipTypeMismatchesPrimitiveClassException;
+import hu.opdoc.openkmip4j.utils.GuardedByteArray;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
 
 /**
  * Created by peter on 2017.06.18..
  */
 public class TtlvSerializer implements KmipSerializer {
+
+    private static final byte[] PADDING = new byte[8];
+
+    static {
+        Arrays.fill(PADDING, (byte)0);
+    }
 
     @Override
     public Primitive readPrimitive(InputStream input) throws IOException, KmipException {
@@ -27,6 +38,9 @@ public class TtlvSerializer implements KmipSerializer {
         writeRecursive(primitive, output);
     }
 
+    /*
+     * Writing section
+     */
     private long writeRecursive(Primitive primitive, OutputStream output) throws IOException, KmipException {
         final Type type = primitive.getType();
         final long valueLength = primitive.getLength();
@@ -38,16 +52,18 @@ public class TtlvSerializer implements KmipSerializer {
         output.write(headerBuffer);
 
         // Value (V)
+        final long writtenBytes;
         switch (type) {
-            case Integer:
+            case Integer: {
                 if (!(primitive instanceof Integer)) {
                     throw new KmipTypeMismatchesPrimitiveClassException(primitive);
                 }
-                writeSignedInteger(((Integer) primitive).getValue(), output);
+                writtenBytes = writeSignedInteger(((Integer) primitive).getValue(), output);
                 break;
+            }
 
             case Enumeration:
-            case Interval:
+            case Interval: {
                 final Long value;
                 if (primitive instanceof Enumeration) {
                     value = ((Enumeration)primitive).getNumericValue();
@@ -57,38 +73,126 @@ public class TtlvSerializer implements KmipSerializer {
                     throw new KmipTypeMismatchesPrimitiveClassException(primitive);
                 }
 
-                writeUnsignedInteger(value, output);
+                writtenBytes = writeUnsignedInteger(value, output);
                 break;
+            }
 
-            case Boolean:
+            case DateTime:
+            case LongInteger: {
+                final Long value;
+                if (primitive instanceof LongInteger) {
+                    value = ((LongInteger)primitive).getValue();
+                } else if (primitive instanceof DateTime) {
+                    value = ((DateTime)primitive).getValue().toEpochSecond();
+                } else {
+                    throw new KmipTypeMismatchesPrimitiveClassException(primitive);
+                }
+                writtenBytes = writeSignedLongInteger(value, output);
+                break;
+            }
+
+            case BigInteger: {
+                if (!(primitive instanceof BigInteger)) {
+                    throw new KmipTypeMismatchesPrimitiveClassException(primitive);
+                }
+                try (final GuardedByteArray guard = ((BigInteger)primitive).getByteArray()) {
+                    output.write(guard.getValue());
+                    writtenBytes = guard.getValue().length;
+                }
+                break;
+            }
+
+            case Boolean: {
                 if (!(primitive instanceof Boolean)) {
                     throw new KmipTypeMismatchesPrimitiveClassException(primitive);
                 }
-                writeBoolean(((Boolean) primitive).getValue(), output);
+                writtenBytes = writeBoolean(((Boolean) primitive).getValue(), output);
                 break;
+            }
 
-            // TODO: Further Primitives...
+            case TextString: {
+                if (!(primitive instanceof TextString)) {
+                    throw new KmipTypeMismatchesPrimitiveClassException(primitive);
+                }
+                try (final GuardedByteArray guardedEncodedBytes = ((TextString)primitive).getEncoded()) {
+                    output.write(guardedEncodedBytes.getValue());
+                    writtenBytes = guardedEncodedBytes.getValue().length;
+                }
+                break;
+            }
+
+            case ByteString: {
+                if (!(primitive instanceof ByteString)) {
+                    throw new KmipTypeMismatchesPrimitiveClassException(primitive);
+                }
+                try (final GuardedByteArray guardedEncodedBytes = ((ByteString)primitive).getValue()) {
+                    output.write(guardedEncodedBytes.getValue());
+                    writtenBytes = guardedEncodedBytes.getValue().length;
+                }
+                break;
+            }
+
+            case Structure: {
+                if (!(primitive instanceof Structure)) {
+                    throw new KmipTypeMismatchesPrimitiveClassException(primitive);
+                }
+                long writtenSoFar = 0;
+                for (final Primitive subPrimitive : ((Structure)primitive).getValue()) {
+                    writtenSoFar += writeRecursive(subPrimitive, output);
+                }
+                if (writtenSoFar < valueLength) {
+                    if (valueLength - writtenSoFar >= 8) {
+                        throw new KmipRuntimeException(
+                                String.format(
+                                        "Wrong calculated length (%d) of a Structure that contains elements of only %d bytes.",
+                                        valueLength,
+                                        writtenSoFar
+                                )
+                        );
+                    }
+                    output.write(PADDING, 0, Long.valueOf(valueLength - writtenSoFar).intValue());
+                }
+                writtenBytes = valueLength;
+                break;
+            }
+
+            default: {
+                throw new KmipRuntimeException(String.format("Unknown primitive type: %s", type));
+            }
+        }
+
+        if (writtenBytes != valueLength) {
+            throw new KmipRuntimeException(
+                    String.format(
+                            "Mismatching value (length of %d) and value length (%d) in the header of a(n) %s primitive.",
+                            writtenBytes,
+                            valueLength,
+                            primitive.getType()
+                    )
+            );
         }
 
         return headerLength + valueLength;
     }
 
     private long writeSignedInteger(java.lang.Integer value, OutputStream output) throws IOException {
-        return writeUnsignedInteger(java.lang.Integer.toUnsignedLong(value), output);
+        output.write(ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(value).array());
+        return 4;
     }
 
     private long writeUnsignedInteger(Long value, OutputStream output) throws IOException {
         final byte[] valueBuffer = new byte[4];
-        valueBuffer[3] = (byte)(value & 0xFF);
-        value >>= 8;
-        valueBuffer[2] = (byte)(value & 0xFF);
-        value >>= 8;
-        valueBuffer[1] = (byte)(value & 0xFF);
-        value >>= 8;
-        valueBuffer[0] = (byte)(value & 0xFF);
-
+        for (int i = 3; i >= 0; i--) {
+            valueBuffer[i] = (byte)(value & 0xFF);
+            value >>= 8;
+        }
         output.write(valueBuffer);
         return 4;
+    }
+
+    private long writeSignedLongInteger(Long value, OutputStream output) throws IOException {
+        output.write(ByteBuffer.allocate(8).order(ByteOrder.BIG_ENDIAN).putLong(value).array());
+        return 8;
     }
 
     private long writeBoolean(boolean value, OutputStream output) throws IOException {
@@ -99,4 +203,8 @@ public class TtlvSerializer implements KmipSerializer {
         output.write(valueBuffer);
         return 8;
     }
+
+    /*
+     * Reading section
+     */
 }
